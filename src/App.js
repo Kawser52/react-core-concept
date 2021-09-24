9afc6c8d7a1530f13d076194c706a94c18a2280f
Soriful Islam Kawser <shoriful521399@gmail.com>
2021-09-24 08:01:50 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Friend name= "Md. Nazmul Islam Tusar" position = "Engineer"></Friend>
+     <Friend name= "Md. Abullah Al Muid" position="UI Designer"></Friend>
+     <Paidcourse name= "Nafiur Rahman Nafi"></Paidcourse>
+     <Paidcourse name= "Md. Shakib"></Paidcourse>
+     <Paidcourse name= "Urmi Akter Mahi"></Paidcourse>
     </div>
   );
 }
 
+function Friend(props){
+  const friendStyle = {
+    backgroundColor : 'blue',
+    padding:'20px',
+    margin: '20px auto',
+    borderRadius: '10px',
+    color:'white',
+    width:'50%',
+
+  }
+  return (
+    <div style = {friendStyle}>
+    <h1>Name: {props.name}</h1>
+    <h4>Pisition: {props.position}</h4>
+    </div>
+  );
+}
+
+function Paidcourse(props){
+   const studentStyle = {
+    backgroundColor : 'skyblue',
+    padding:'20px',
+    margin: '20px auto',
+    borderRadius: '10px',
+    color:'white',
+    width:'50%',
+
+  }
+  return(
+    <div style = {studentStyle}>
+      <h2>Student Name: {props.name}</h2>
+    </div>
+  );
+}
 export default App;
